17bf73800df92932b8351a6ff02fd4080540cd36
DhrupamP <pdhrupam@gmail.com>
2021-06-23 09:05:25 +0400

    modify homepage

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:spotify_clone/styles/constants.dart';
 
@@ -10,7 +11,51 @@ class HomePage extends StatelessWidget {
       body: SafeArea(
         child: Container(
           decoration: const BoxDecoration(
-            color: kBlack,
+        gradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment(0.0000001, -0.5), // 10% of the width, so there are ten blinds.
+        colors: <Color>[
+          Color(0x88FFFF8F),
+          Color(0xff000000),
+          Color(0xff000000),
+          Color(0xff000000),
+
+              ],
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              //SizedBox(height: 100,),
+              Row(
+                children: [
+                  Text(' Good Evening',style: kHeadingTextStyle,),
+                  SizedBox(width: 110,),
+                  Icon(Icons.restore,color: Colors.white,size: 30,),
+                  SizedBox(width:20,),
+                  Icon(Icons.settings,color: Colors.white,size: 30,),
+                ],
+              ),
+              // Expanded(
+              //   child: ListView(
+              //     children: [
+              //       Container(
+              //         color: Colors.white30,
+              //         child: Row(
+              //           crossAxisAlignment: CrossAxisAlignment.stretch,
+              //           children: [
+              //             Icon(Icons.photo,color: Colors.white,),
+              //             Text('Daily Mix 1')
+              //           ],
+              //         ),
+              //       ),
+              //
+              //     ],
+              //   ),
+              // )
+
+            ],
           ),
         ),
       ),
