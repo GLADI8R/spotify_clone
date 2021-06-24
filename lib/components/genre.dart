081fcce720c9705b0362485c207e39797039ac57
GLADI8R <danishmehmuda@gmail.com>
2021-06-24 12:02:51 +0530

    Add Genre Cards

--- lib/components/genre.dart
+++ lib/components/genre.dart
@@ -1,34 +1,19 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:spotify_clone/components/genre_card.dart';
 import 'package:spotify_clone/styles/constants.dart';
 
 GridView genreCard = GridView.count(
   crossAxisSpacing: 15.0,
-  mainAxisSpacing: 10.0,
+  mainAxisSpacing: 150.0,
   childAspectRatio: (1 / 0.55), //set the height of card
   shrinkWrap: true,
   crossAxisCount: 2,
   children: [
-    Container(
-      padding: const EdgeInsets.all(10.0),
-      decoration: const BoxDecoration(
-        color: Colors.purple,
-        borderRadius: BorderRadius.all(Radius.circular(5.0)),
-      ),
-      child: const Text(
-        'Pop',
-        style: kCardTextStyle,
-      ),
-    ),
-    Container(
-      padding: const EdgeInsets.all(10.0),
-      decoration: const BoxDecoration(
-        color: Colors.orange,
-        borderRadius: BorderRadius.all(Radius.circular(5.0)),
-      ),
-      child: const Text(
-        'Hip Hop',
-        style: kCardTextStyle,
-      ),
-    ),
+    const GenreCard(Color(0xff8d67ab), 'Pop'),
+    const GenreCard(Color(0xffba5d07), 'Hip-Hop'),
+    const GenreCard(Color(0xffdc148c), 'Dance/Electronic'),
+    const GenreCard(Color(0xffff4632), 'Bollywood'),
   ],
 );
--- lib/components/genre_card.dart
+++ lib/components/genre_card.dart
@@ -5,22 +5,24 @@ import 'package:flutter/widgets.dart';
 import 'package:spotify_clone/styles/constants.dart';
 
 class GenreCard extends StatelessWidget {
-  final Color _cardColor;
-  final String _cardText;
+  final Color cardColor;
+  final String cardText;
 
   // const GenreCard({ Key? key }, this.cardColor, this.cardText) : super(key: key);
-  const GenreCard(this._cardColor, this._cardText);
+  const GenreCard(this.cardColor, this.cardText);
+
+  // final String finalColor = cardColor.toString();
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(10.0),
-      decoration: const BoxDecoration(
-        color: _cardColor!=Null?_cardColor:Colors.black,
-        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+      decoration: BoxDecoration(
+        color: cardColor,
+        borderRadius: const BorderRadius.all(Radius.circular(5.0)),
       ),
       child: Text(
-        _cardText,
+        cardText,
         style: kCardTextStyle,
       ),
     );
