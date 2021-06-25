a8cfd5c5745aea6cb4a68ebe983eb5f30c256aec
DhrupamP <pdhrupam@gmail.com>
2021-06-25 08:55:27 +0400

    modify homescreen

--- lib/components/home_page_card_1.dart
+++ lib/components/home_page_card_1.dart
@@ -1,30 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:spotify_clone/styles/constants.dart';
+import 'package:spotify_clone/screens/home_page.dart';
 
 class HomePageCard1 extends StatelessWidget {
   HomePageCard1({required this.cardtext});
 
   final String cardtext;
 
+
   @override
   Widget build(BuildContext context) {
+    double screenwidth = MediaQuery. of(context). size. width;
     return Container(
-      padding: EdgeInsets.symmetric(vertical: 5,horizontal: 15),
+      padding: EdgeInsets.symmetric(vertical: 0,horizontal: 0),
       margin: EdgeInsets.only(left: 8,right: 5,top: 10,bottom: 5),
-      height: 70,
-      width: 200,
+      height: 55,
+      width: screenwidth*0.5,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-        color: kGrey,
+        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+        color: Colors.grey.withOpacity(0.5)
       ),
       child: Row(
         children: [
-          CircleAvatar(
-            radius: 50,
-            backgroundColor: kGrey,
-            child: Image.asset('assets/images/73775.png'),
-          ),
-          Text(cardtext,style: TextStyle(color: Colors.white),)
+          Image.asset('assets/images/73775.png'),
+          Container(
+            child: Text(
+              cardtext,
+              style: TextStyle(color: Colors.white),
+              softWrap: true,
+              textAlign: TextAlign.left,
+            ),
+          )
         ],
       ),
     );
