33f281dc54372cdba7b459221a312f5e44ccf46a
GLADI8R <danishmehmuda@gmail.com>
2021-06-26 09:14:40 +0530

    Add Gesture Detector

--- lib/components/genre_card.dart
+++ lib/components/genre_card.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors
+// ignore_for_file: use_key_in_widget_constructors, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -13,15 +13,22 @@ class GenreCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 92.0,
-      padding: const EdgeInsets.all(10.0),
-      decoration: BoxDecoration(
-        color: cardColor,
-        borderRadius: const BorderRadius.all(Radius.circular(5.0)),
-      ),
-      child: Text(
-        cardText,
-        style: kCardTextStyle,
+      child: GestureDetector(
+        onTap: () {
+          print('Card pressed');
+        },
+        child: Container(
+          height: 92.0,
+          padding: const EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+            color: cardColor,
+            borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+          ),
+          child: Text(
+            cardText,
+            style: kCardTextStyle,
+          ),
+        ),
       ),
     );
   }
