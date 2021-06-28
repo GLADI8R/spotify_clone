54aafa971fcbc8d1ace7891536fef88bd00f646c
GLADI8R <danishmehmuda@gmail.com>
2021-06-28 09:26:59 +0530

    Minor changes

--- lib/screens/library_page.dart
+++ lib/screens/library_page.dart
@@ -10,7 +10,6 @@ class LibraryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
         child: Container(
