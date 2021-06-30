2eb4ec9e87ef835ad39d3bfad1eab9a7fa2c54c5
GLADI8R <danishmehmuda@gmail.com>
2021-06-30 11:34:10 +0530

    Add Player Status Bar

--- lib/components/navbar.dart
+++ lib/components/navbar.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, sized_box_for_whitespace
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:spotify_clone/styles/constants.dart';
 import 'package:spotify_clone/screens/home_page.dart';
@@ -29,29 +32,84 @@ class _NavigationBarState extends State<NavigationBar> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _widgetOptions[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+      backgroundColor: kDarkGrey,
+      persistentFooterButtons: [
+        Container(
+          height: 45.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/square.webp',
+                height: 50.0,
+              ),
+              const SizedBox(width: 10.0),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Song Name',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14.0),
+                  ),
+                  const Text(
+                    'Artist',
+                    style: TextStyle(color: kLightGrey, fontSize: 10.0),
+                  ),
+                  // const Divider(color: Colors.black),
+                ],
+              ),
+              const Spacer(),
+              const Icon(
+                Icons.favorite_border,
+                color: Colors.white,
+                size: 30.0,
+              ),
+              const SizedBox(
+                width: 10.0,
+              ),
+              const Icon(
+                Icons.play_arrow,
+                color: Colors.white,
+                size: 30.0,
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search_sharp),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list),
-            label: 'Library',
-          ),
-          // BottomNavigationBarItem(
-          //   icon: Icon(Premium icon)
-          // ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.white,
-        unselectedItemColor: kLightGrey,
-        backgroundColor: kDarkGrey,
-        onTap: _onItemTapped,
+        )
+      ],
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          color: Colors.black,
+          border: Border(top: BorderSide(color: Colors.black, width: 1.0)),
+        ),
+        child: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search_sharp),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              label: 'Library',
+            ),
+            // BottomNavigationBarItem(
+            //   icon: Icon(Premium icon)
+            // ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: kLightGrey,
+          backgroundColor: kDarkGrey,
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
