9dbda63aefceddd79905b1f280e5b72924d81955
GLADI8R <danishmehmuda@gmail.com>
2021-07-01 10:18:15 +0530

    Add splash screen

--- lib/components/splash_screen.dart
+++ lib/components/splash_screen.dart
@@ -1,78 +1,21 @@
-import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:spotify_clone/components/navbar.dart';
 import 'package:spotify_clone/styles/constants.dart';
+import 'package:splashscreen/splashscreen.dart';
 
-// ignore: use_key_in_widget_constructors
-class SplashScreen extends StatefulWidget {
-  @override
-  SplashScreenState createState() => SplashScreenState();
-}
-
-class SplashScreenState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
-  var _visible = true;
-
-  late AnimationController animationController;
-  late Animation<double> animation;
-
-  startTime() async {
-    var _duration = const Duration(seconds: 3);
-    return Timer(_duration, navigationPage);
-  }
-
-  void navigationPage() {
-    Navigator.of(context).pushReplacementNamed('/nav');
-  }
-
-  @override
-  dispose() {
-    animationController.dispose();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 4),
-    );
-    animation =
-        CurvedAnimation(parent: animationController, curve: Curves.easeOut);
-
-    animation.addListener(() => setState(() {}));
-    animationController.forward();
-
-    setState(() {
-      _visible = !_visible;
-    });
-    startTime();
-  }
+class SplashScreenSpotify extends StatelessWidget {
+  const SplashScreenSpotify({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return SplashScreen(
+      seconds: 6,
+      navigateAfterSeconds: const NavigationBar(),
+      image: Image.asset('assets/images/spotifypng.png'),
+      photoSize: 100.0,
       backgroundColor: kBlack,
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            mainAxisSize: MainAxisSize.min,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.asset(
-                'assets/images/spotifypng.png',
-                width: animation.value * 250,
-                height: animation.value * 250,
-              ),
-            ],
-          ),
-        ],
-      ),
+      loaderColor: kBlack,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
+// @dart=2.9
 import 'package:flutter/material.dart';
 import 'package:spotify_clone/components/navbar.dart';
-import 'package:spotify_clone/screens/home_page.dart';
 import 'package:spotify_clone/screens/player_screen.dart';
 import 'package:spotify_clone/screens/playlist_songs_screen.dart';
 import 'components/splash_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -22,7 +20,7 @@ class MyApp extends StatelessWidget {
         '/songslist': (context) => SongsList(),
         '/player': (context) => const MusicPlayer(),
         '/nav': (context) => const NavigationBar(),
-        '/splashscreen': (context) => SplashScreen()
+        '/splashscreen': (context) => const SplashScreenSpotify()
       },
     );
   }
