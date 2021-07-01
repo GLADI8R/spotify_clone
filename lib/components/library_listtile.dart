b05dd66adae133b2befc670dbfa1d67ebefbaf57
DhrupamP <pdhrupam@gmail.com>
2021-07-01 09:43:34 +0400

    modify library acreen

--- lib/components/library_listtile.dart
+++ lib/components/library_listtile.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:spotify_clone/styles/constants.dart';
+import 'package:spotify_clone/main.dart';
+import 'package:spotify_clone/screens/playlist_songs_screen.dart';
 
 class LibraryListTile extends StatelessWidget {
   const LibraryListTile(
@@ -17,43 +19,48 @@ class LibraryListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        ClipRRect(
-          child: Image.asset(
-            listImage,
-            height: 70.0,
-            width: 70.0,
+    return GestureDetector(
+      onTap: (){
+        Navigator.pushNamed(context, '/songslist');
+      },
+      child: Row(
+        children: [
+          ClipRRect(
+            child: Image.asset(
+              listImage,
+              height: 70.0,
+              width: 70.0,
+            ),
           ),
-        ),
-        const SizedBox(
-          width: 16.0,
-        ),
-        Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 19.0),
-                child: Text(
-                  listTitle,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 17.0,
+          const SizedBox(
+            width: 16.0,
+          ),
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 19.0),
+                  child: Text(
+                    listTitle,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 17.0,
+                    ),
                   ),
                 ),
-              ),
-              Text(
-                listAuthor,
-                style: const TextStyle(
-                  color: kLightGrey,
-                  fontSize: 13.0,
+                Text(
+                  listAuthor,
+                  style: const TextStyle(
+                    color: kLightGrey,
+                    fontSize: 13.0,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
